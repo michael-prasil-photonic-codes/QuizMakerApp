0db80597994a9f431cc5809469110f8c0b7397b9
Michael Prasil <michael.prasil@photonic-codes.com>
2023-08-16 14:59:59 +0200

    Implemented the first steps for the quiz maker component

--- src/app/quiz-maker/quiz-maker.component.ts
+++ src/app/quiz-maker/quiz-maker.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import { Categories, Difficulty, TriviaService } from '../trivia.service';
 
 @Component({
   selector: 'app-quiz-maker',
   templateUrl: './quiz-maker.component.html',
   styleUrls: ['./quiz-maker.component.scss']
 })
-export class QuizMakerComponent {
+export class QuizMakerComponent implements AfterViewInit {
+  public categories!: Categories;
+  public difficulties!: string[];
+
+  public constructor(private triviaService: TriviaService) {
+  }
+
+  public ngAfterViewInit(): void {
+    this.triviaService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+
+    this.difficulties = Object.keys(Difficulty);
+  }
+
+  public createQuiz(): void {
+  }
 }
