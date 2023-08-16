97b7b8abda972a32a40dfa255535ca4c8b7fd116
Michael Prasil <michael.prasil@photonic-codes.com>
2023-08-16 13:38:14 +0200

    Generated quiz maker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { QuestionComponent } from './question/question.component';
+import { QuizMakerComponent } from './quiz-maker/quiz-maker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizComponent,
-    QuestionComponent
+    QuestionComponent,
+    QuizMakerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quiz-maker/quiz-maker.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-maker',
+  templateUrl: './quiz-maker.component.html',
+  styleUrls: ['./quiz-maker.component.scss']
+})
+export class QuizMakerComponent {
+}
