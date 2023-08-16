dd905df92500ddba921319f5330632fa90be928a
Michael Prasil <michael.prasil@photonic-codes.com>
2023-08-16 14:07:54 +0200

    Requesting categories in service

--- src/app/trivia.service.ts
+++ src/app/trivia.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Categories {
+  trivia_categories: Category[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TriviaService {
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public getCategories() {
+    return this.http.get<Categories>('https://opentdb.com/api_category.php');
+  }
+
+  public getQuestions() {
+  }
 }
