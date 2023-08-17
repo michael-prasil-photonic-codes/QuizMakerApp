dc15256d139937c6b872bbf1fff1f43e9b59f9cc
Michael Prasil <michael.prasil@photonic-codes.com>
2023-08-17 22:25:10 +0200

    Moved some logic to the service

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -11,18 +11,29 @@ import { Difficulty } from './difficulty.model';
 export class QuizService {
   private games: QuizGame[] = [];
 
-  public currentGameIndex!: number;
-  public currentGame!: QuizGame;
+  public currentGameIndex: number | undefined;
+  public currentGame: QuizGame | undefined;
 
   constructor(private triviaService: TriviaService) {
   }
 
   public createQuiz(category: Category, difficulty: Difficulty): void {
-    this.currentGame = new QuizGame();
-    this.games.push(this.currentGame);
-    this.currentGameIndex = this.games.length - 1;
     this.triviaService.getQuestions(category, difficulty).subscribe((questions) => {
+      this.currentGame = new QuizGame();
+      this.games.push(this.currentGame);
+      this.currentGameIndex = this.games.length - 1;
       this.currentGame.questions = questions;
     });
+  }
+
+  public setAnswer(index: number, answer: string | undefined): void {
+    if (this.currentGame) {
+      this.currentGame.answers[index] = answer ? answer : '';
+    }
+  }
+
+  public resetQuiz(): void {
+    this.currentGame = undefined;
+    this.currentGameIndex = undefined;
   }
 }
