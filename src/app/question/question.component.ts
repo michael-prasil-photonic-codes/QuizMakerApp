0912f0cd2386bf23d23591f7fb34a0c9e5d0ff1d
Michael Prasil <michael.prasil@photonic-codes.com>
2023-08-17 17:45:44 +0200

    Removed return in durstenfeld shuffle

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -20,14 +20,13 @@ export class QuestionComponent implements AfterViewInit {
     this.durstenfeldShuffle(this.answers);
   }
 
-  private durstenfeldShuffle<T>(array: T[]): T[] {
+  private durstenfeldShuffle<T>(array: T[]): void {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = array[i];
       array[i] = array[j];
       array[j] = temp;
     }
-    return array;
   }
 
   public selectAnswer(answer: string): void {
