988e77f57abede3b216ab52b143e6ce80a258809
Michael Prasil <michael.prasil@photonic-codes.com>
2023-08-17 19:32:11 +0200

    Implemented template and component for the results component

--- src/app/quiz-results/quiz-results.component.ts
+++ src/app/quiz-results/quiz-results.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Questions } from '../questions.model';
 
 @Component({
   selector: 'app-quiz-results',
   templateUrl: './quiz-results.component.html',
   styleUrls: ['./quiz-results.component.scss']
 })
-export class QuizResultsComponent {
+export class QuizResultsComponent implements AfterViewInit {
+  public questions!: Questions;
+
+  public constructor(private router: Router) {
+  }
+
+  public ngAfterViewInit(): void {
+  }
+
+  public createQuiz(): void {
+  }
 }
